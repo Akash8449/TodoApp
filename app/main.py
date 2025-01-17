f4dfdd1cc1fc00bbd472f07cc0f322fb97fab6ba
Akash8449 <akashgujjar8449@gmail.com>
2025-01-17 17:26:40 +0530

    redirect docs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from routers import tasks
 
 app = FastAPI()
- 
+
 app.include_router(tasks.router)
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Todo App"}
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
